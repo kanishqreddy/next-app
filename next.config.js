1d5ca8c791e68fc3551178be035e0ad29e539a9e
Kanishq reddy <44079524+kanishqreddy@users.noreply.github.com>
2025-11-30 20:54:40 +1100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = { reactStrictMode: true };
 module.exports = nextConfig;
+
+module.exports = {
+  reactStrictMode: true,
+  headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors *",
+          },
+        ],
+      },
+    ];
+  },
+};
