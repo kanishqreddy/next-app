ea28a64ccbfa7335d21523b2f53ff28a05ebce07
Kanishq reddy <44079524+kanishqreddy@users.noreply.github.com>
2025-11-30 23:25:40 +1100

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -1,10 +1,22 @@
 import Link from 'next/link'
 
-
 export default function Nav(){
-return (
-<nav style={{padding:16, borderBottom:'1px solid #eee'}}>
-<Link href="/">Home</Link> | <Link href="/dashboard">Dashboard</Link> | <Link href="/login">Login</Link> | <Link href="/admin">Admin</Link> | <Link href="/pay">Pay</Link>
-</nav>
-)
+  return (
+    <nav style={{
+      padding:16,
+      background:"#050507",
+      borderBottom:"1px solid #1b1c25",
+      color:"#f9fafb",
+      display:"flex",
+      gap:"12px",
+      justifyContent:"center",
+      fontSize:"14px"
+    }}>
+      <Link href="/">Home</Link>
+      <Link href="/dashboard">Dashboard</Link>
+      <Link href="/login">Login</Link>
+      <Link href="/admin">Admin</Link>
+      <Link href="/pay">Pay</Link>
+    </nav>
+  )
 }
